e8d0b0180a77365934bac5d09884b1fbc0a8167e
tomvanmele <tom.v.mele@gmail.com>
2025-01-31 07:51:34 +0100

    simplify buffer generation and docs stubs

--- src/compas_dem/notebook/modelobject.py
+++ src/compas_dem/notebook/modelobject.py
@@ -1,69 +1,13 @@
 from typing import Optional
 
-import numpy
 import pythreejs as three
 from compas.colors import Color
-from compas.datastructures import Mesh
-from compas.geometry import Polygon
-from compas.geometry import earclip_polygon
 from compas_notebook.scene import ThreeSceneObject
 
 from compas_dem.models import BlockModel
 
-
-def mesh_to_edgesbuffer(mesh: Mesh, color: Color):
-    positions = []
-    colors = []
-
-    for u, v in mesh.edges():
-        positions.append(mesh.vertex_coordinates(u))
-        positions.append(mesh.vertex_coordinates(v))
-        colors.append(color)
-        colors.append(color)
-
-    return positions, colors
-
-
-def mesh_to_facesbuffer(mesh: Mesh, color: Color):
-    positions = []
-    colors = []
-
-    for face in mesh.faces():
-        vertices = mesh.face_vertices(face)
-
-        if len(vertices) == 3:
-            positions.append(mesh.vertex_coordinates(vertices[0]))
-            positions.append(mesh.vertex_coordinates(vertices[1]))
-            positions.append(mesh.vertex_coordinates(vertices[2]))
-            colors.append(color)
-            colors.append(color)
-            colors.append(color)
-
-        elif len(vertices) == 4:
-            positions.append(mesh.vertex_coordinates(vertices[0]))
-            positions.append(mesh.vertex_coordinates(vertices[1]))
-            positions.append(mesh.vertex_coordinates(vertices[2]))
-            colors.append(color)
-            colors.append(color)
-            colors.append(color)
-            positions.append(mesh.vertex_coordinates(vertices[0]))
-            positions.append(mesh.vertex_coordinates(vertices[2]))
-            positions.append(mesh.vertex_coordinates(vertices[3]))
-            colors.append(color)
-            colors.append(color)
-            colors.append(color)
-
-        else:
-            ears = earclip_polygon(Polygon([mesh.vertex_coordinates(v) for v in vertices]))
-            for ear in ears:
-                positions.append(mesh.vertex_coordinates(vertices[ear[0]]))
-                positions.append(mesh.vertex_coordinates(vertices[ear[1]]))
-                positions.append(mesh.vertex_coordinates(vertices[ear[2]]))
-                colors.append(color)
-                colors.append(color)
-                colors.append(color)
-
-    return positions, colors
+from .buffers import meshes_to_edgesbuffer
+from .buffers import meshes_to_facesbuffer
 
 
 class ThreeBlockModelObject(ThreeSceneObject):
@@ -124,141 +68,26 @@ class ThreeBlockModelObject(ThreeSceneObject):
 
         return self.guids
 
-    def draw_blockfaces(self):
-        positions = []
-        colors = []
+    def draw_blockfaces(self) -> three.Mesh:
+        meshes = [block.modelgeometry for block in self.model.blocks()]
+        return meshes_to_facesbuffer(meshes, Color(0.9, 0.9, 0.9))
 
-        for block in self.model.blocks():
-            buffer = mesh_to_facesbuffer(block.modelgeometry, Color(0.9, 0.9, 0.9))
-            positions += buffer[0]
-            colors += buffer[1]
+    def draw_blockedges(self) -> three.LineSegments:
+        meshes = [block.modelgeometry for block in self.model.blocks()]
+        return meshes_to_edgesbuffer(meshes, Color(0.2, 0.2, 0.2))
 
-        positions = numpy.array(positions, dtype=numpy.float32)
-        colors = numpy.array(colors, dtype=numpy.float32)
+    def draw_supportfaces(self) -> three.Mesh:
+        meshes = [block.modelgeometry for block in self.model.supports()]
+        return meshes_to_facesbuffer(meshes, Color.red().lightened(50))
 
-        geometry = three.BufferGeometry(
-            attributes={
-                "position": three.BufferAttribute(positions, normalized=False),
-                "color": three.BufferAttribute(colors, normalized=False, itemSize=3),
-            }
-        )
-        material = three.MeshBasicMaterial(
-            side="DoubleSide",
-            vertexColors="VertexColors",
-        )
-        return three.Mesh(geometry, material)
-
-    def draw_blockedges(self):
-        positions = []
-        colors = []
-
-        for block in self.model.blocks():
-            buffer = mesh_to_edgesbuffer(block.modelgeometry, Color(0.2, 0.2, 0.2))
-            positions += buffer[0]
-            colors += buffer[1]
-
-        positions = numpy.array(positions, dtype=numpy.float32)
-        colors = numpy.array(colors, dtype=numpy.float32)
-
-        geometry = three.BufferGeometry(
-            attributes={
-                "position": three.BufferAttribute(positions, normalized=False),
-                "color": three.BufferAttribute(colors, normalized=False, itemSize=3),
-            }
-        )
-        material = three.LineBasicMaterial(vertexColors="VertexColors")
-        return three.LineSegments(geometry, material)
-
-    def draw_supportfaces(self):
-        positions = []
-        colors = []
-
-        for block in self.model.supports():
-            buffer = mesh_to_facesbuffer(block.modelgeometry, Color.red().lightened(50))
-            positions += buffer[0]
-            colors += buffer[1]
-
-        positions = numpy.array(positions, dtype=numpy.float32)
-        colors = numpy.array(colors, dtype=numpy.float32)
-
-        geometry = three.BufferGeometry(
-            attributes={
-                "position": three.BufferAttribute(positions, normalized=False),
-                "color": three.BufferAttribute(colors, normalized=False, itemSize=3),
-            }
-        )
-        material = three.MeshBasicMaterial(
-            side="DoubleSide",
-            vertexColors="VertexColors",
-        )
-        return three.Mesh(geometry, material)
-
-    def draw_supportedges(self):
-        positions = []
-        colors = []
-
-        for block in self.model.supports():
-            buffer = mesh_to_edgesbuffer(block.modelgeometry, Color.red())
-            positions += buffer[0]
-            colors += buffer[1]
-
-        positions = numpy.array(positions, dtype=numpy.float32)
-        colors = numpy.array(colors, dtype=numpy.float32)
-
-        geometry = three.BufferGeometry(
-            attributes={
-                "position": three.BufferAttribute(positions, normalized=False),
-                "color": three.BufferAttribute(colors, normalized=False, itemSize=3),
-            }
-        )
-        material = three.LineBasicMaterial(vertexColors="VertexColors")
-        return three.LineSegments(geometry, material)
+    def draw_supportedges(self) -> three.LineSegments:
+        meshes = [block.modelgeometry for block in self.model.supports()]
+        return meshes_to_edgesbuffer(meshes, Color(0.2, 0.2, 0.2))
 
     def draw_contactfaces(self):
-        color = Color.cyan().lightened(75)
-
-        positions = []
-        colors = []
-
-        for contact in self.model.contacts():
-            buffer = mesh_to_facesbuffer(contact.polygon.to_mesh(), color)
-            positions += buffer[0]
-            colors += buffer[1]
-
-        positions = numpy.array(positions, dtype=numpy.float32)
-        colors = numpy.array(colors, dtype=numpy.float32)
-
-        geometry = three.BufferGeometry(
-            attributes={
-                "position": three.BufferAttribute(positions, normalized=False),
-                "color": three.BufferAttribute(colors, normalized=False, itemSize=3),
-            }
-        )
-        material = three.MeshBasicMaterial(
-            side="DoubleSide",
-            vertexColors="VertexColors",
-        )
-        return three.Mesh(geometry, material)
+        meshes = [contact.polygon.to_mesh() for contact in self.model.contacts()]
+        return meshes_to_facesbuffer(meshes, Color.cyan().lightened(75))
 
     def draw_contactedges(self):
-        color = Color(0.2, 0.2, 0.2)
-
-        positions = []
-        colors = []
-
-        for contact in self.model.contacts():
-            buffer = mesh_to_edgesbuffer(contact.polygon.to_mesh(), color)
-            positions += buffer[0]
-            colors += buffer[1]
-
-        positions = numpy.array(positions, dtype=numpy.float32)
-        colors = numpy.array(colors, dtype=numpy.float32)
-
-        geometry = three.BufferGeometry(
-            attributes={
-                "position": three.BufferAttribute(positions, normalized=False),
-                "color": three.BufferAttribute(colors, normalized=False, itemSize=3),
-            }
-        )
-        material = three.LineBasicMaterial(vertexColors="VertexColors")
-        return three.LineSegments(geometry, material)
+        meshes = [contact.polygon.to_mesh() for contact in self.model.contacts()]
+        return meshes_to_edgesbuffer(meshes, Color(0.2, 0.2, 0.2))
