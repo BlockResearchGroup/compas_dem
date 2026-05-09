bd8e6aee7bfb9f3a74cf5e12a63390d5f1d027eb
Baraa El Moussa <belmoussa@ethz.ch>
2026-05-09 11:36:12 +0200

    Removed dem_viewer from docs

--- scripts/DEM_Analysis_workflow/2_Blocks/301_SW_Viz.py
+++ scripts/DEM_Analysis_workflow/2_Blocks/301_SW_Viz.py
@@ -16,20 +16,20 @@ problem = compas.json_load(
 # Visualize block results
 # =============================================================================
 
-# graph = problem.model.graph
-# for node in graph.nodes():
-#     block_transformation = graph.node_attribute(node, "transformation")
-#     # print(f"Block {node} transformation:\n{block_transformation}\n")
+graph = problem.model.graph
+for node in graph.nodes():
+    block_transformation = graph.node_attribute(node, "transformation")
+    # print(f"Block {node} transformation:\n{block_transformation}\n")
 
-# for edge in graph.edges():
-#     gap = graph.edge_attribute(edge, "gap")
-#     magnitude = graph.edge_attribute(edge, "force_magnitude")
-#     print(f"Edge {edge} gap: {gap}, force magnitude: {magnitude}")
+for edge in graph.edges():
+    gap = graph.edge_attribute(edge, "gap")
+    magnitude = graph.edge_attribute(edge, "force_magnitude")
+    print(f"Edge {edge} gap: {gap}, force magnitude: {magnitude}")
 
 
-# # =============================================================================
-# # Visualize problem
-# # =============================================================================
+# =============================================================================
+# Visualize problem
+# =============================================================================
 
 viewer = DEMViewer(problem.model)
 viewer.add_solution(scale=0.5)
